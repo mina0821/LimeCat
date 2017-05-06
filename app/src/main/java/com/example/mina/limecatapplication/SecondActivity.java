478e23f08d3c796567b3d73ff722955586ebb598
Mingnan Su <sum1@mcmaster.ca>
2017-05-05 23:09:28 -0400

    Basic UI and activity

--- app/src/main/java/com/example/mina/limecatapplication/SecondActivity.java
+++ app/src/main/java/com/example/mina/limecatapplication/SecondActivity.java
@@ -7,16 +7,25 @@ import android.widget.TextView;
 
 
 public class SecondActivity extends Activity {
-    public final static String MSG_KEY = "com.example.mina.limecatapplication.message_key";
+    public final static String MSG_KEY = "Date";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //get the message
         Intent intent = getIntent();
         String msg = intent.getStringExtra(MSG_KEY);
+        String msg2 = intent.getStringExtra("Time");
+        String msg3 = intent.getStringExtra("Model");
+
+        //create a textview variable of current window
         TextView textView = new TextView(this);
+
+        //set the size of the font
         textView.setTextSize(45);
-        textView.setText(msg);
+
+        //send the message to view
+        textView.setText(msg+"\n"+msg2+"\n"+msg3);
         setContentView(textView);
 
         //setContentView(R.layout.second_layout);
--- app/src/main/java/com/example/mina/limecatapplication/WelcomeActivity.java
+++ app/src/main/java/com/example/mina/limecatapplication/WelcomeActivity.java
@@ -1,6 +1,7 @@
 package com.example.mina.limecatapplication;
 
 import android.content.Intent;
+import android.support.annotation.IdRes;
 import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,31 +12,77 @@ import android.view.*;
 public class WelcomeActivity extends AppCompatActivity {
     TextView textView;
     EditText msg_text;
-    public final static String MSG_KEY = "com.example.mina.limecatapplication.message_key";
+    public final static String MSG_KEY = "Date";
+
+    private RadioGroup radioTimeGroup;
+    private RadioGroup radioModelGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        textView = (TextView) findViewById(R.id.hell_text_view);
+        //locate the textview box we want to show the result
+        textView = (TextView)findViewById(R.id.selectTime);
+
+        //add a listener to the radio button group
+        addListenerOnButton();
     }
 
-    public void showHell(View view){
-        String button_text = ((Button) view).getText().toString();
+    public void addListenerOnButton(){
+        radioTimeGroup = (RadioGroup) findViewById(R.id.timeGroup);
+        radioModelGroup = (RadioGroup) findViewById(R.id.modelGroup);
 
-        if (button_text.equals("Open Window...")){
-            Intent intent = new Intent(this,ThirdActivity.class);
-            startActivity(intent);
-        }
-        textView.setText(button_text);
+        //attach listener to radio group
+        radioTimeGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                //get the right button which is clicked
+                RadioButton rb = (RadioButton) group.findViewById(checkedId);
+                if (null != rb && checkedId > -1){
+                    Toast.makeText(WelcomeActivity.this, rb.getText(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        //attach listener to radio group
+        radioModelGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                //get the right button which is clicked
+                RadioButton rb = (RadioButton) group.findViewById(checkedId);
+                if (null != rb && checkedId > -1){
+                    Toast.makeText(WelcomeActivity.this, rb.getText(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
+    //after we hit show button
     public void sendMessage(View view){
+        //store the msg in the text box
         msg_text = (EditText)findViewById(R.id.msg_text);
         String msg = msg_text.getText().toString();
+
+        //call second window
         Intent intent = new Intent(this,SecondActivity.class);
+        //input the msg we get
         intent.putExtra(MSG_KEY,msg);
+
+        //get the selection of time period
+        RadioButton rb = (RadioButton) radioTimeGroup.findViewById(radioTimeGroup.getCheckedRadioButtonId());
+        String time = (String) rb.getText();
+
+        //get the selection of climate model
+        RadioButton rb2 = (RadioButton) radioModelGroup.findViewById(radioModelGroup.getCheckedRadioButtonId());
+        String model = (String) rb2.getText();
+
+        //input the selection we get
+        intent.putExtra("Time",time);
+        intent.putExtra("Model",model);
+
+        //start the activity
         startActivity(intent);
+
     }
 
 }
